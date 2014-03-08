bf0a97ff6eb2769da011032185e1ff220227f028
blazejhadzik <blazej.hadzik@netguru.pl>
2014-03-08 11:05:56 +0100

    init

---
+++ pickadate/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pickadate::Application.config.secret_key_base = '633e3b1d8fa2df579b16aac7360dadd361fa8d6c55d506d5a7b8abb91f667ff126d09063440ed1786351c1d2ed9d05b74db9c0d8eab4a875318d34b79c2fdea8'
