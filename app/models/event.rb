eaa9c80cf25840a1810946cc0da8f569ee1f6819
barti03 <barmalanczuk@gmail.com>
2014-03-08 14:48:33 +0100

    added creator to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event
   include Mongoid::Document
 
   has_many :users_events
+  has_one  :creator , class_name: 'User', inverse_of: nil
   embeds_one :duration
 
   accepts_nested_attributes_for :duration
