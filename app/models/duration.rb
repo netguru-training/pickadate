e51a5cc3c11ab79ed957a9533680017e67044066
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-09 14:47:48 +0100

    validate event length

--- app/models/duration.rb
+++ app/models/duration.rb
@@ -5,12 +5,17 @@ class Duration
 
   validates :start_date, :end_date, presence: true
   validate :validate_time_format
+  validate :validate_length
 
   field :start_date, type: DateTime
   field :end_date, type: DateTime
 
   def validate_time_format
     errors.add(:end_date, "Please add date after start time") if end_date <= start_date
+  end
+
+  def validate_length
+    errors.add(:start_date, "Event can't be longer than 48 hours") if (end_date - start_date).to_i > 2
   end
 
 end
