986ab069a9ed196a95a774df4d4865bab8355db1
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-09 13:53:27 +0100

    nicer event page

--- app/decorators/users_event_decorator.rb
+++ app/decorators/users_event_decorator.rb
@@ -4,4 +4,8 @@ class UsersEventDecorator < Draper::Decorator
   def cell_from_availability avaliability
     avaliability.zero? ? 'remove' : 'ok'
   end
+
+  def cell_class avaliability
+    avaliability.zero? ? 'danger' : 'success'
+  end
 end
