be7cdfca9a2dc17d1a154136da564d2f1151f6a7
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-09 11:45:47 +0100

    check if user can see event

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -9,6 +9,10 @@ class EventDecorator < Draper::Decorator
     parse_date(self.duration.end_date)
   end
 
+  def is_visible_for_user? user
+    !self.invite_only || self.is_created_by?(user) || self.guests.include?(user)
+  end
+
   private
 
   def parse_date date
