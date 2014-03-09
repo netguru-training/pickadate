8f0af41fbfa24903356338e18987cbe24406eb35
Kamil Wreczycki <kamil.wreczycki@allegro.pl>
2014-03-09 14:55:46 +0100

    update & flash message

--- app/controllers/users_events_controller.rb
+++ app/controllers/users_events_controller.rb
@@ -4,11 +4,30 @@ class UsersEventsController < ApplicationController
 
   def create
     users_event = UsersEvent.new(user: current_user, event_id: params[:event_id])
-    users_event.availability = params[:users_event][:avaibility].split(',').map(&:to_i)
-    users_event.save
+    assign_avaibility
+    save_users_event
+
     redirect_to event_path(params[:event_id])
   end
 
   def update
+    assign_avaibility
+    save_users_event
+
+    redirect_to event_path(params[:event_id])
+  end
+
+  def save_users_event
+    if users_event.save
+      flash[:success] = "Succesfully updated !"
+    else
+      flash[:danger] = "Update failed !"
+    end
+  end
+
+  private
+
+  def assign_avaibility
+    users_event.availability = params[:users_event][:avaibility].split(',').map(&:to_i)
   end
 end
